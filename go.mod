94c714b0b2130c936f24b850eaaeda738740107a
Andrei Georgescu <ageorgescu98@gmail.com>
2021-06-11 22:30:08 -0700

    now following go package best practices

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/doxify/supreme-go-api
 
 go 1.16
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/doxify/supreme-go-api/supreme"
+)
+
 func main() {
-    s := CreateNewInstance()
-    s.FetchStock()
+	s := supreme.CreateNewInstance()
+	s.FetchStock()
+
+	p := s.LookupCategoryProduct("panel", "hats")
+	for _, product := range *p {
+		spew.Dump(product)
+	}
 }
