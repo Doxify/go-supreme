5cc4de18a145f113e37d6192ab265d46f8ae8af9
Andrei Georgescu <ageorgescu98@gmail.com>
2021-06-18 19:55:17 -0700

    can now fetch product data(styles) from supreme

--- examples/main.go
+++ examples/main.go
@@ -1,8 +1,30 @@
 package main
 
-import gosupreme "github.com/doxify/supreme-go-api"
+import (
+	"fmt"
+	"log"
+
+	gosupreme "github.com/doxify/supreme-go-api"
+)
 
 func main() {
 	s := gosupreme.New()
 	s.Init()
+
+	p, err := s.GetProductsByKeyword("6-panel")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for _, product := range *p {
+		fmt.Print(product)
+		d, err := s.FetchProductData(product)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		fmt.Println(d)
+	}
+
 }
