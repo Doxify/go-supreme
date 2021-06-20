294787d0970723a6551b7b7c6825880ce26b8632
Andrei Georgescu <ageorgescu98@gmail.com>
2021-06-20 12:30:14 -0700

    better documented main example

--- examples/main.go
+++ examples/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	gosupreme "github.com/doxify/gosupreme"
 )
@@ -11,21 +10,27 @@ func main() {
 	s := gosupreme.New()
 	s.Init()
 
-	p, err := s.GetProductsByKeyword("Boxer Briefs (2 Pack)")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Get all products that have the keyword 'Boxer Briefs (2 Pack)' in their
+	// name.
+	products, _ := s.GetProductsByKeyword("Boxer Briefs (2 Pack)")
 
-	for _, product := range *p {
-		s.FetchProductData(product)
-		styles, _ := s.GetAllStyles(product)
-		style, _ := styles.GetStyleByColor("purple")
-		size, _ := style.GetSize("medium")
+	// Select the first one from the list of returned products
+	var product = (*products)[0]
 
-		fmt.Println(product)
-		fmt.Println(style)
-		fmt.Println(size)
+	// Fetch that products data (styles, sizes, etc...)
+	s.FetchProductData(product)
 
-	}
+	// Get all of the product's styles (these are the different colors/variants
+	// the products come in)
+	styles, _ := s.GetAllStyles(product)
 
+	// Get a style that is the color 'purple'
+	style, _ := styles.GetStyleByColor("purple")
+
+	// Get the size that is 'medium'
+	size, _ := style.GetSize("medium")
+
+	fmt.Println(product)
+	fmt.Println(style)
+	fmt.Println(size)
 }
