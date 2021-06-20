125a1d9249e7310520e6d0f5496fc14885200b81
Andrei Georgescu <ageorgescu98@gmail.com>
2021-06-20 11:17:01 -0700

    implemented product category enums

--- const.go
+++ const.go
@@ -1,7 +1,43 @@
 package gosupreme
 
-// Endpoint
+// Endpoints
 const (
 	StockURL = "https://supremenewyork.com/mobile_stock.json"
 	DataURL  = "https://supremenewyork.com/shop/%d.json"
 )
+
+// Product categories registry.
+// This registry represents all valid supreme product categories.
+var ProductCategory = newCategoryRegistry()
+
+func newCategoryRegistry() *categoryRegistry {
+	return &categoryRegistry{
+		Accessories:  "accessories",
+		Bags:         "bags",
+		Skate:        "skate",
+		TopsSweaters: "tops/sweaters",
+		Pants:        "pants",
+		Shirts:       "shirts",
+		Shorts:       "shorts",
+		Hats:         "hats",
+		Sweatshirts:  "sweatshirts",
+		Jackets:      "jackets",
+		Shoes:        "shoes",
+		new:          "new",
+	}
+}
+
+type categoryRegistry struct {
+	Accessories  string
+	Bags         string
+	Skate        string
+	TopsSweaters string
+	Pants        string
+	Shirts       string
+	Shorts       string
+	Hats         string
+	Sweatshirts  string
+	Jackets      string
+	Shoes        string
+	new          string
+}
---
+++ style.go
@@ -0,0 +1,49 @@
+package gosupreme
+
+// Represents a product's style
+type Style struct {
+	ID                    int         `json:"id"`
+	Name                  string      `json:"name"`
+	Chk                   string      `json:"chk"`
+	Tag                   interface{} `json:"tag"`
+	Currency              string      `json:"currency"`
+	Description           string      `json:"description"`
+	ImageURL              string      `json:"image_url"`
+	ImageURLHi            string      `json:"image_url_hi"`
+	SwatchURL             string      `json:"swatch_url"`
+	SwatchURLHi           string      `json:"swatch_url_hi"`
+	MobileZoomedURL       string      `json:"mobile_zoomed_url"`
+	MobileZoomedURLHi     string      `json:"mobile_zoomed_url_hi"`
+	BiggerZoomedURL       string      `json:"bigger_zoomed_url"`
+	SpecialPurchasableQty interface{} `json:"special_purchasable_qty"`
+	Sizes                 Sizes       `json:"sizes"`
+}
+
+type Styles []*Style
+
+// Represents a product style size
+type Size struct {
+	Name       string `json:"name"`
+	ID         int    `json:"id"`
+	StockLevel int    `json:"stock_level"`
+}
+
+type Sizes []*Size
+
+// Returns a product's styles
+func (s *Supreme) GetAllStyles(p *Product) (*Styles, error) {
+	// check if the style is cached
+	if s.Data[p.ID] != nil {
+		return &s.Data[p.ID].Styles, nil
+	}
+
+	//  otherwise fetch it
+	pd, err := s.FetchProductData(p)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pd.Styles, nil
+}
+
+// func (s *Supreme) GetStyle
